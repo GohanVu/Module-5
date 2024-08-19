6d4b7863d7e7608004bb62cb9dfd52529c4260bc
GohanVu <hoangvhdata@gmail.com>
2024-08-19 14:10:23 +0700

    practice function component

---
+++ ss3/function_component/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import AddComponent from "./components/AddComponent";
+
+function App() {
+  return (
+    <AddComponent firstNumber={3} secondNumber={4}></AddComponent>
+  );
+}
+
+export default App;
