d45a8589dfae70813304db9a6a066a3700df9c8d
GohanVu <hoangvhdata@gmail.com>
2024-08-20 16:40:25 +0700

    exercise_car_selection

---
+++ ss4/exercise_car_selection/src/components/Selector.js
@@ -0,0 +1,34 @@
+import {useState} from "react";
+
+const carList =['Vinfast','BMW', 'Lexus'];
+const colorList = ['Black', 'White', 'Red']
+function Selector (){
+    const [selectedCar,setSelectedCar] = useState({car:carList[0],color : colorList[0]});
+    const handleCarChange = (event) => {
+        setSelectedCar(prevState => {
+            return {
+                ...prevState, car: event.target.value
+            }
+        })
+    }
+    const handleColorChange = (event) => {
+        setSelectedCar(prevState => {
+            return{
+                ...prevState, color: event.target.value
+            }
+        })
+    }
+    return (
+        <div>
+            <select onChange={handleCarChange}>
+                {carList.map((car, index) => <option key={index} value={car}>{car}</option>)}
+            </select>
+            <br/>
+            <select onChange={handleColorChange}>
+                {colorList.map((color, index) => <option key={index} value={color}>{color}</option>)}
+            </select>
+            <p>You selected a {selectedCar.color} - {selectedCar.car}</p>
+        </div>
+    )
+}
+export default  Selector;
