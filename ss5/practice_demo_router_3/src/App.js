fbd88c830509ca6ae0acba134e2d8b9a0b03005d
GohanVu <hoangvhdata@gmail.com>
2024-08-21 09:44:50 +0700

    practice_demo_router_3

---
+++ ss5/practice_demo_router_3/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Category from "./components/Category";
+import Product from "./components/Product";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Category />} />
+          <Route path="/product" element={<Product />} />
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
